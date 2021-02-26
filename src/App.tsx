0dbf66397b876287d89d7e2870379684ef5615d0
MaxKlammer <maximilian.klammer@betssongroup.com>
2021-02-26 18:07:29 +0100

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { adverbs } from "./data";
+import { verbs } from "./data";
+import { adjectives } from "./data";
+import { nouns } from "./data";
 
+function pickRandomValue(words: string[], min = 0) {
+  const value = Math.floor(Math.random() * (words.length + 1 - min) + min); //The maximum is exclusive and the minimum is inclusive
+  return words[value];
+}
 function App() {
+  const adverb = pickRandomValue(adverbs);
+  const verb = pickRandomValue(verbs);
+  const adjective = pickRandomValue(adjectives);
+  const noun = pickRandomValue(nouns);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{adverb}</h1>
+      <h1>{verb}</h1>
+      <h1>{adjective}</h1>
+      <h1>{noun}</h1>
     </div>
   );
 }
---
+++ src/data/index.ts
@@ -0,0 +1,4 @@
+export { default as adverbs } from "./adverbs";
+export { default as adjectives } from "./adjectives";
+export { default as nouns } from "./nouns";
+export { default as verbs } from "./verbs";
